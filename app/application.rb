70c14f39e8c613d7961888baada3e1135a499823
Kevin Chi <kevinchi118@gmail.com>
2018-07-24 15:38:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,13 @@
 def Application
+    @@item
+
+
     def call(env)
         resp = Rack::Response.new
         req = Rack::Request.new(env)
 
         if req.path.match(/songs/)
             item_name = req.path.split("/songs/").last
-            item = Item::
+            item = Item.
     end
 end
